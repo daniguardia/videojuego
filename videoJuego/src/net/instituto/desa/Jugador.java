4c81945a68470ca0da9f0ebf74581e49d4d823e7
Daniel Guardia <daniguardiamail@gmail.com>
2020-02-23 22:00:04 +0100

    refactorizado

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -18,5 +18,21 @@ public class Jugador {
 	public void setX(int x) {
 		this.x = x;
 	}
+	
+	public void movmientoDerecha() {
+		this.x++;
+	}
+	
+	public void movmientoIzquierda() {
+		this.x--;
+	}
+
+	public void movmientoArriba() {
+		this.y--;
+	}
+
+	public void movmientoAbajo() {
+		this.y++;
+	}
 
 }
